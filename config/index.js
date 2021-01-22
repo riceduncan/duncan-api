4d37c6a4d01c726b46b2572b2f77c2c382a90643
Andy Wang <andynmnwang@gmail.com>
2021-01-22 17:49:37 -0500

    add auth secret

--- config/index.js
+++ config/index.js
@@ -7,6 +7,8 @@ let config = {
 
 config.port = process.env.PORT || '8080'
 
+config.auth.secret = 'lMaO1!1'
+
 config.db.name = process.env.DB_NAME || 'dev'
 config.db.uri = process.env.DB_URI || ''
 
