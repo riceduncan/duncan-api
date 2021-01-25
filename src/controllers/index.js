5f3122ba30ae410bd4655a33a4eb7b2f8d2f3eb8
Andy Wang <andynmnwang@gmail.com>
2021-01-24 22:26:58 -0500

    add event routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/auth', require('./auth.controller'))
+router.use('/events', require('./event.controller'))
+router.use('/users', require('./user.controller'))
 
 module.exports = router
